e6e909211d855e409ff649804ffa09f29c15988c
9905Makoto <tanukispring1515@gmail.com>
2025-01-25 13:06:04 +0900

    食材選択画面 ヘッダー作成中 20% #5

--- app/web/src/components/ui/header.tsx
+++ app/web/src/components/ui/header.tsx
@@ -79,21 +79,11 @@ export default function Header() {
             <span className="sr-only">Tabeyo</span>
             <img alt="Tabeyo logo" src={tabeyoLogo} className="h-8 w-auto" />
           </a>
-          <div className="-m-2.5 p-1.5 font-bold tracking-tight text-gray-900">
+          <div className="-m-2.5 p-1.5 text-2xl font-bold tracking-tight text-gray-900">
             Tabeyo
           </div>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+        {/* <PopoverGroup className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
               Product
@@ -150,20 +140,19 @@ export default function Header() {
             </PopoverPanel>
           </Popover>
 
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Features
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Marketplace
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Company
-          </a>
-        </PopoverGroup>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900"></a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900"></a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900"></a>
+        </PopoverGroup> */}
+        <div className="flex justify-end">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 bg-[#82C05A]"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-10" />
+          </button>
         </div>
       </nav>
       <Dialog
